3c7fc9ae58f0651fe67271e416d83299861b4b82
Felipe <felipe.lima.liincooln@gmail.com>
2023-06-13 00:33:20 -0300

    recipe view

--- src/pages/detail/index.js
+++ src/pages/detail/index.js
@@ -1,13 +1,19 @@
-import { useLayoutEffect } from 'react';
-import { View, Text, StyleSheet, Pressable } from 'react-native';
+import { useLayoutEffect, useState } from 'react';
+import { View, Text, StyleSheet, Pressable, ScrollView, Image, Modal, Share } from 'react-native';
 import { useRoute, useNavigation } from '@react-navigation/native';
 
-import { Entypo } from '@expo/vector-icons';
+import { Entypo, AntDesign, Feather } from '@expo/vector-icons';
+
+import { Ingredients } from '../../components/ingredients'
+import { Instructions } from '../../components/instructions'
+import { VideoView } from '../../components/video';
 
 export function Detail() {
 
     const route = useRoute();
     const navigation = useNavigation();
+
+    const [showVideo, setShowVideo] = useState(false);
 
     //CONDICIONAL PARA RENDERIZAR O NOME DO ITEM SELECIONADO NO HEADER E FAVORITAR.
     useLayoutEffect(()=> {
@@ -27,16 +33,116 @@ export function Detail() {
 
     }, [navigation, route.params?.data])
 
+    function handleOpenVideo(){
+        setShowVideo(true);
+    }
+
+    async function shareReceipe(){
+        try {
+           await Share.share({
+            uri: route.params?.data.video,
+            message: `Receita: ${route.params?.data.name}\nAcabei de ver no app receita fácil`, 
+           })
+        } catch (error) {
+           console.log(error);
+        }
+    }
+
     return(
-        <View style={styles.container}>
-        <Text>{route.params?.data.name}</Text>
+<ScrollView contentContainerStyle={{paddingBottom: 14}} style={styles.container} showsVerticalScrollIndicator={false}>
+
+  <Pressable onPress={handleOpenVideo}>
+    <View style={styles.playIcon}>
+        <AntDesign name="playcircleo" size={48} color='#FAFAFA'/>
+    </View>
+    <Image
+    source={{uri: route.params?.data.cover}}
+    style={styles.cover}
+    />
+  </Pressable>
+
+        <View style={styles.hearderDetails}>  
+         <View>       
+            <Text style={styles.title}>{route.params?.data.name}</Text>
+            <Text style={styles.ingredientsText}>Ingredientes ({route.params?.data.total_ingredients})</Text>
+         </View>
+         <Pressable onPress={shareReceipe}>
+            <Feather name='share-2' size={24} color='#121212' />
+         </Pressable> 
         </View>
+
+        {route.params?.data.ingredients.map( (item)=> (
+            <Ingredients key={item.id} data={item}/>
+        ))}
+
+        <View style={styles.instructionsArea}>
+            <Text style={styles.instructionsText}>Modo de preparo</Text>
+            <Feather name="arrow-down" size={24} color="#FFF" />
+        </View>
+
+        {
+        route.params?.data.instructions.map((item, index) => (
+            <Instructions key={item.id} data={item} index={index}/>
+        ))}
+
+        <Modal visible={showVideo} animationType="slide">
+         <VideoView
+          handleClose={ () => setShowVideo(false) }
+          videoUrl={route.params?.data.video}
+         />
+        </Modal>
+
+</ScrollView>
     )
 }
 
 const styles = StyleSheet.create({
     container:{
-        flex: 1,
-        backgroundColor: '#8823'
-    }
+        backgroundColor: '#F3f9ff',
+        paddingTop: 14,
+        paddingEnd: 14,
+        paddingStart: 14
+    },
+    cover:{
+        height: 200,
+        borderRadius: 14,
+        width: '100%',
+    },
+    playIcon:{
+        position: 'absolute',
+        zIndex: 9,
+        top: 0, left: 0, right: 0, bottom:0,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    title:{
+        fontSize: 18,
+        marginTop: 14,
+        fontWeight: 'bold',
+        color: '#000',
+        marginTop: 4
+    },
+    ingredientsText:{
+        marginBottom: 14,
+        fontSize: 16
+    },
+    hearderDetails:{
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginBottom: 14
+    },
+    instructionsArea:{
+        backgroundColor: "#4CBE6C",
+        flexDirection: 'row',
+        padding: 8,
+        borderRadius: 4,
+        marginBottom: 14
+    },
+    instructionsText:{
+        fontSize: 18,
+        fontWeight: 500,
+        color: "#FFF",
+        marginRight: 8
+    },
 })
